0b665ed2238e7edc81953efaea92a865daf781a3
kaliweh <khalidaliweh@gmail.com>
2021-05-02 22:33:07 -0400

    allow all

--- sequence-alignment/app.js
+++ sequence-alignment/app.js
@@ -141,7 +141,7 @@ exports.lambdaHandler = async (event, context) => {
             'statusCode': 200,
             'headers': {
                 "Access-Control-Allow-Headers" : "Content-Type",
-                "Access-Control-Allow-Origin": "http://localhost:3000,*",
+                "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
             },
             'body': JSON.stringify({
