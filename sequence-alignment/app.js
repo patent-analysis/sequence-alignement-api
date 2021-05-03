c3d4bf9b39056671795ca4765a763e763e9b1cf2
kaliweh <khalidaliweh@gmail.com>
2021-05-02 22:12:58 -0400

    allow cors

--- sequence-alignment/app.js
+++ sequence-alignment/app.js
@@ -140,9 +140,9 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'headers': {
-                "Access-Control-Allow-Origin":  "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "http://localhost:3000,*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
             },
             'body': JSON.stringify({
                 message: resp,
